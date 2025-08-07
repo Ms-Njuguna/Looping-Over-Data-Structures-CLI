a2c685407fd530acea25885d9d0516343761f44e
Patricia <patricianjuguna084@gmail.com>
2025-08-07 15:12:53 +0300

    Challenge 3 complete: use for loop and range() to calculate sum of integers in user imput

--- practice.py
+++ practice.py
@@ -6,3 +6,14 @@ while count > 0:
     print(count) 
     count -= 1
 print("Blast off!")
+
+
+# challenge 2
+
+num = int(input("Give me a number: "))
+total = 0
+
+for i in range(1, num):
+    total += i
+print(f"The total sum is: {total}")
+
